35d7399b4c69ff8ca4a1e119e16a22e2af4b2fcd
lindatech <hayashidaster@gmail.com>
2023-02-14 16:07:02 +0900

    hero.phpの作成

---
+++ basic_php_v3-master/work/hero.php
@@ -0,0 +1,71 @@
+<?php 
+
+$n = mt_rand(1, 3);
+
+if ($n === 1){
+  $message = "I'm on training";
+} elseif ($n === 2){
+  $message = "Am I in exponential grouth?";
+} else {
+  $message = 'Today is'. ' ' .date('l');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>lindatech portfolio</title>
+  <link rel="icon" href="favicon.ico">
+  <meta name="description" content="lindatechのポートフォリオサイトです">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header>
+    <div class="container">
+      <div class="icon">
+        <img src="img/taro.png" width="120" height="120" alt="太郎のアイコンです">
+      </div>
+      <div class="info">
+        <h1>Shunya Hayashida</h1>
+        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
+        <ul>
+          <li>
+            <a href="https://dotinstall.com" target="_blank">
+              <img src="img/blog.png" width="20" height="20" alt="ブログサイトへのリンク画像です">
+            </a>
+          </li>
+          <li>
+            <a href="https://dotinstall.com" target="_blank">
+              <img src="img/photos.png" width="20" height="20" alt="写真サイトへのリンク画像です">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+
+  <section class="works">
+    <h1>WORKS</h1>
+
+    <section>
+      <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
+      <h1>勇者ゲーム</h1>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
+      
+      <a herf="index.php">戻る</a>
+    </section>
+
+  </section>
+
+  <footer>
+    <p>(c) dotinstall.com</p>
+  </footer>
+</body>
+</html>
+  
