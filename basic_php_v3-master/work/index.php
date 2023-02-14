f6409c2abc1a5365ee50a4e94e5dc0293965a74d
lindatech <hayashidaster@gmail.com>
2023-02-14 15:58:15 +0900

    Make hero.php

--- basic_php_v3-master/work/index.php
+++ basic_php_v3-master/work/index.php
@@ -49,24 +49,26 @@ if ($n === 1){
   <section class="works">
     <h1>WORKS</h1>
     <section>
-      <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
-      <h1>勇者ゲーム</h1>
-      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-      <a herf="hero.php">もっと見る</a>
+      <img src="img/hirameki.png" width="400" height="260" alt="Hiramekiの紹介画像">
+      <h1>Hirameki(バグリスト)</h1>
+      <h2>Ideate/Product owner</h2>
+      <p>「新規事業のアイデアが浮かばない」「ブレインストーミングをしても煮詰まってしまう」「新規事業部を立ち上げたは良いがうまく行かない」こんなことはありませんか？「バグリスト」は一定時間に日常のバグ（悩みや怒り、苦痛に関していること）をひたすら書きなぐる究極のアイデア発想法です。こんなにシンプルなことをするだけで新しいビジネスの核を作ることができます。</p>
+      <a herf="https://hirameki.cocolofun.com/buglist/">もっと見る</a>
     </section>
   
     <section>
-      <img src="img/work2.png" width="400" height="260" alt="宝探しゲームの紹介画像">
-      <h1>宝探しゲーム</h1>
-      <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-      <a herf="tresure.php">もっと見る</a>
+      <img src="img/bizlean.png" width="400" height="260" alt="BizLean">
+      <h1>BizLean</h1>
+      <h2>Ideate/Product owner</h2>
+      <p>アントレプレナーのための事業創造支援メディアとして、新規事業担当者や起業家を応援するため、新規事業創出の科学的フレームワークを提供しています。</p>
+      <a herf="http://bizlean.cocolofun.com/">もっと見る</a>
     </section>
   
     <section>
       <img src="img/work3.png" width="400" height="260" alt="神経衰弱の紹介画像">
       <h1>神経衰弱</h1>
       <p>楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。楽しいアプリです。</p>
-      <a herf="memory.php">もっと見る</a>
+      <a herf="hero.php">もっと見る</a>
     </section>
 
   </section>
